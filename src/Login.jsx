c4c379a00f79c3a37fca521ee534de9cfe0fb0b3
Jad-Atout <JadAtout.2003@outlook.sa>
2024-09-17 00:06:19 +0300

    first commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -10,6 +10,16 @@ export default function Login() {
         const data = await axios.post('https://ecommerce-node4.onrender.com/auth/signin',formik.values)
         console.log('hi')
 
+        },
+        validate:(values)=>{
+        let errors = {}
+            if(values.email.length <10){
+                errors.email = "Enter a valid email"
+            }
+            if(values.password.length <5){
+                errors.password = "Enter a valid password"
+            }
+            return errors
         }
     })
     return (
@@ -18,11 +28,22 @@ export default function Login() {
                 <form onSubmit={formik.handleSubmit}>
                     <div className={'mb-3 d-flex'}>
                         <label htmlFor={'email'}>User Email</label>
-                        <input type={'email'} className={'form-control w-auto'} id={'email'} value={formik.values.email} onChange={formik.handleChange}/>
+                        <input type={'email'}
+                               className={'form-control w-auto'}
+                               id={'email'} value={formik.values.email}
+                               onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+                        {(formik.touched.email && formik.errors.email)?<div className={'alert alert-danger '}>{formik.errors.email}</div>:null}
                     </div>
                     <div className={'mb-3 d-flex'}>
                         <label htmlFor={'password'}>Password</label>
-                        <input type={'password'} className={'form-control w-auto'} id={'password'} value={formik.values.password} onChange={formik.handleChange}/>
+                        <input type={'password'}
+                               className={'form-control w-auto'}
+                               id={'password'}
+                               value={formik.values.password}
+                               onChange={formik.handleChange}
+                               onBlur={formik.handleBlur}
+                        />
+                        {(formik.touched.password && formik.errors.password)?<div className={'alert alert-danger '}>{formik.errors.password}</div>:null}
                     </div>
                     <button type={'submit'} className={'btn btn-info'}>submit</button>
 
--- src/Register.jsx
+++ src/Register.jsx
@@ -9,6 +9,20 @@ export default function Register() {
             password:""
         }, onSubmit:async (values)=>{
          const data = await axios.post('https://ecommerce-node4.onrender.com/auth/signup',values)
+        },
+        validate:(values)=>{
+        let errors = {}
+            if(values.userName.length<3){
+                errors.userName = "Enter a valid user name"
+            }
+            if(values.email.length<10){
+                errors.email = "Enter a valid user email"
+            }
+            if(values.password.length<5){
+                errors.password = "Enter a valid user password"
+            }
+            return errors
+
         }})
 
     return (
@@ -17,15 +31,35 @@ export default function Register() {
                     <div>
                     <div className={'mb-3 d-flex align-items-center '}>
                     <label htmlFor={'userName'} className={'form-label fw-bold'}>User Name</label>
-                    <input type={'text'} className={`form-control w-auto mx-2`} onChange={formik.handleChange} value={formik.values.userName} id={'userName'}/>
+                    <input type={'text'}
+                           className={`form-control w-auto mx-2`}
+                           onChange={formik.handleChange}
+                           value={formik.values.userName}
+                           id={'userName'}
+                            onBlur={formik.handleBlur}/>
+                        {(formik.touched.userName && formik.errors.userName)?<div className={'alert alert-danger'}>{formik.errors.userName}</div>:null}
                     </div>
                     <div className={'mb-3 d-flex align-items-center'}>
                     <label htmlFor={'email'} className={'form-label fw-bold'}>Email</label>
-                    <input type={'email'} className={'form-control w-auto mx-2'} onChange={formik.handleChange} value={formik.values.email}id={'email'}/>
+                    <input type={'email'}
+                           className={'form-control w-auto mx-2'}
+                           onChange={formik.handleChange}
+                           value={formik.values.email}
+                           id={'email'}
+                           onBlur={formik.handleBlur}
+                    />
+                        {(formik.touched.email && formik.errors.email)?<div className={'alert alert-danger'}>{formik.errors.email}</div>:null}
                     </div>
                     <div className={'mb-3 d-flex align-items-center'}>
                     <label htmlFor={'password'} className={'form-label fw-bold'}>Password</label>
-                    <input type={'password'} className={'form-control w-auto mx-2'} onChange={formik.handleChange} value={formik.values.password} id={'password'}/>
+                    <input type={'password'}
+                           className={'form-control w-auto mx-2'}
+                           onChange={formik.handleChange}
+                           value={formik.values.password}
+                           id={'password'}
+                           onBlur={formik.handleBlur}
+                    />
+                        {(formik.touched.password && formik.errors.password)?<div className={'alert alert-danger'}>{formik.errors.password}</div>:null}
                     </div>
                         <button type={'submit'} className={'btn btn-info d-inline'}>Submit</button>
                     </div>
